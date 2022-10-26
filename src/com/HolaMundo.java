6d0006295ab06d00f7258522645970fb61930a64
Wilmer Peñafiel <109515122+wilmerpe@users.noreply.github.com>
2022-10-26 20:11:31 +0200

    Update HolaMundo.java

--- src/com/HolaMundo.java
+++ src/com/HolaMundo.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class HolaMundo {
     public static void main(String[] args) {
+        //El sistema Scanner en este caso lo utilizamos para pedir al usuario un valor
         Scanner entrada = new Scanner(System.in);
         float nota1, nota2, nota3, resultadoSuma;
 
@@ -17,3 +18,13 @@ public class HolaMundo {
         System.out.println("\nLa suma es: " + resultadoSuma);
     }
 }
+
+ /*
+        Los operadores de suma,resta,multiplicación, división y el resto de una división son de esta manera en
+        el lenguaje Java:
+        Suma = +
+        Resta = -
+        Multiplicación = *
+        División = /
+        Resto = %
+         */
